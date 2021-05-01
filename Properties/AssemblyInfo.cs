663fbe860b530c9584a193659c94db8b0b8643e7
shassingh <shassingh@teksystems.com>
2021-05-01 18:57:50 +0530

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JoePizzaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JoePizzaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ee9c356-2455-41b7-a259-8cfb3bceb789")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace JoePizzaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://shashwatsapp.azurewebsites.net/");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                var cheeseburst = driver.FindElementById("add-1");
+                Assert.IsNotNull(cheeseburst);
+                cheeseburst.Click();
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+
+                var cartbtn = driver.FindElementById("cart-btn");
+                Assert.IsNotNull(cartbtn);
+                cartbtn.Click();
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+                var result = driver.PageSource.Contains("Cheese burst");
+                Assert.IsTrue(result);
+                var placeorderbuton = driver.FindElementById("order-btn");
+                Assert.IsNotNull(placeorderbuton);
+                placeorderbuton.Click();
+                var alert = driver.SwitchTo().Alert();
+                var orderstatus = alert.Text;
+                Assert.AreEqual("order successful", orderstatus);
+            }
+        }
+    }
+}
